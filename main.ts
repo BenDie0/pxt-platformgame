ecc325ed3891511881d42d61688de887ddc7f07b
BenDie0 <58610677+BenDie0@users.noreply.github.com>
2019-12-17 13:50:40 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,146 @@
+namespace SpriteKind {
+    export const object = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+}
+scene.onHitWall(SpriteKind.Player, function (sprite) {
+    game.over(false, effects.confetti)
+})
+sprites.onOverlap(SpriteKind.object, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    joe.vy += -250
+    trampo.setKind(SpriteKind.object)
+    trampo.x += Math.randomRange(-70, 70)
+    trampo.y += Math.randomRange(-50, -60)
+})
+let trampo: Sprite = null
+let joe: Sprite = null
+scene.setBackgroundImage(img`
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+`)
+joe = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . f 7 7 7 7 7 f . . . . 
+. . . . f 7 7 1 1 1 7 7 f . . . 
+. . . . f 7 7 1 f 1 7 7 f . . . 
+. . . . f 7 7 7 7 7 7 7 f . . . 
+. . . . f 7 7 7 7 7 7 7 f . . . 
+. . . . f f f f f f f f f . . . 
+. . . . . f 2 2 2 2 2 f . . . . 
+. . . . . f 4 2 2 2 f . . . . . 
+. . . . . . f 5 2 4 f . . . . . 
+. . . . . . . f 4 5 f . . . . . 
+. . . . . . . f 5 f . . . . . . 
+. . . . . . . . f . . . . . . . 
+`, SpriteKind.Player)
+joe.ay = 10000
+trampo = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f f f f f f 1 f f f 1 f f f f 1 
+1 f f f 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f 1 f 1 1 1 1 1 1 1 1 f 1 f f 
+f 1 1 f f f f f f f f f f f 1 f 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.object)
+trampo = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f f f f f f 1 f f f 1 f f f f 1 
+1 f f f 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f 1 f 1 1 1 1 1 1 1 1 f 1 f f 
+f 1 1 f f f f f f f f f f f 1 f 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.object)
+trampo = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f f f f f f 1 f f f 1 f f f f 1 
+1 f f f 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f 1 f 1 1 1 1 1 1 1 1 f 1 f f 
+f 1 1 f f f f f f f f f f f 1 f 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.object)
+game.onUpdate(function () {
+    joe.x += controller.dx()
+})
